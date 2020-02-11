98d4947be61157c1d337f551735078774dfacb81
Kirill Vereshchagin <kirill.vereschagin@gmail.com>
2020-02-10 18:41:40 -0700

    added NGinX with custom config

---
+++ nginx/bin/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [[ -n "${WORKER_PROCESSES}" ]] ; then
+  sed -i "s/worker_processes  1;/worker_processes  ${WORKER_PROCESSES};/g" /etc/nginx/nginx.conf ;
+fi
+if [[ -n "${WORKER_CONNECTIONS}" ]] ; then
+  sed -i "s/worker_connections  1024;/worker_connections  ${WORKER_CONNECTIONS};/g" /etc/nginx/nginx.conf ;
+fi
+if [[ ! -z "CUSTOM_CONFIG" ]] ; then
+  echo ${CUSTOM_CONFIG} | base64 -d > /etc/nginx/conf.d/custom.conf
+fi
+
+exec "$@"
